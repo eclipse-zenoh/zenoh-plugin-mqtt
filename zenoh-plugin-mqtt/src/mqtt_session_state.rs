9722d3326193718c85c81eeefa5c8600b1f15d20
Geoff Martin <geoff.martin@zettascale.tech>
2024-06-07 14:22:14 +0100

    Fixing deadlock when bridging MQTT clients.

--- zenoh-plugin-mqtt/src/mqtt_session_state.rs
+++ zenoh-plugin-mqtt/src/mqtt_session_state.rs
@@ -149,7 +149,7 @@ fn route_zenoh_to_mqtt(
         sample.key_expr,
         topic
     );
-    tx.send_blocking((topic, sample.payload.contiguous().to_vec().into()))
+    tx.try_send((topic, sample.payload.contiguous().to_vec().into()))
         .map_err(|e| {
             zerror!(
                 "MQTT client {}: error re-publishing on MQTT a Zenoh publication on {}: {}",
